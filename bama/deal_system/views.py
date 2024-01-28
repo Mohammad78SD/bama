2b16057cd65b91f846fdad6e97322c2c35a004fb
MohammadSD <mohammadsd2000@gmail.com>
2024-01-28 18:03:50 +0330

    feat: preparing cars list html template

--- bama/deal_system/views.py
+++ bama/deal_system/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import car
 # Create your views here.
+
+
+def list(request):
+    cars = car.objects.all()
+    cars_list = []
+    for car in cars:
+        dict = {
+            "brand" : car.brand,
+            "name" : car.name,
+            "km" : car.km,
+            "color" : car.color,
+            "description" : car.description,
+            "image" : car.image,
+        }
+        cars_list.append(dict)
+        
